fcc940cd9cf641737ebfa0b4e24758779e11506b
EddieED <aolaeddie@gmail.com>
2022-01-30 00:04:26 +0800

    Test bug fixes

--- src/events/voiceStateUpdate.js
+++ src/events/voiceStateUpdate.js
@@ -1,4 +1,4 @@
-const { getVoiceConnection } = require('@discordjs/voice');
+const { getVoiceConnection, AudioPlayerStatus } = require('@discordjs/voice');
 const { retrieveData, setData } = require('../utils/changeData.js');
 const client = require('../index.js');
 const { neturalEmbed, successEmbed } = require('../objects/embed.js');
@@ -14,7 +14,7 @@ module.exports = {
 		console.log(
 			`Detected Voice Channel Changes in ${
 				client.guilds.cache.get(newState.guild.id).name
-			}: ${newState.member.user.username} has joined.`
+			}: ${newState.member.user.username} has joined / left.`
 		);
 		const guildId = newState.guild.id;
 		const connection = getVoiceConnection(guildId);
@@ -56,49 +56,55 @@ module.exports = {
 			} else if (voiceChannel.members.size > 1) {
 				const player = cacheData['player'][guildId];
 				if (player) {
-					const unpause = player.unpause();
-					if (unpause) {
-						const channelId = await retrieveData(
-							guildId,
-							'respondChannelId'
-						);
-						if (channelId) {
-							const channel =
-								client.channels.cache.get(channelId);
-							if (channel) {
-								let embed = successEmbed(
-									'Welcome back!',
-									'Smoothie will continue to sing.'
-								);
-								await channel
-									.send({
-										embeds: [embed.embed],
-										files: embed.files,
-									})
-									.catch((err) => {
-										console.error(err);
-									});
-							}
-						}
-					} else {
-						const queue = await retrieveData(guildId, 'queue');
-
-						if (queue.length >= 1) {
-							const resource = await createAudioResource(
-								queue[0]['url'],
-								queue[0]['title']
+					if (player.state.status != AudioPlayerStatus.Playing) {
+						const unpause = player.unpause();
+						if (unpause) {
+							const channelId = await retrieveData(
+								guildId,
+								'respondChannelId'
 							);
-							if (resource != null) {
-								const connection = getVoiceConnection(guildId);
-								if (connection) {
-									const newPlayer = createAudioPlayer(
-										guildId,
-										connection
+							if (channelId) {
+								const channel =
+									client.channels.cache.get(channelId);
+								if (channel) {
+									let embed = successEmbed(
+										'Welcome back!',
+										'Smoothie will continue to sing.'
 									);
-									connection.subscribe(newPlayer);
-									newPlayer.play(resource);
+									await channel
+										.send({
+											embeds: [embed.embed],
+											files: embed.files,
+										})
+										.catch((err) => {
+											console.error(err);
+										});
+								}
+							}
+						} else {
+							const queue = await retrieveData(guildId, 'queue');
 
-									await wait(1000);
+							if (queue.length >= 1) {
+								const resource = await createAudioResource(
+									queue[0]['url'],
+									queue[0]['title']
+								);
+								if (resource != null) {
+									const connection =
+										getVoiceConnection(guildId);
+									if (connection) {
+										const newPlayer = createAudioPlayer(
+											guildId,
+											connection
+										);
+										connection.subscribe(newPlayer);
+										console.log(
+											'New song is created become someone joined.'
+										);
+										newPlayer.play(resource);
+
+										await wait(1000);
+									}
 								}
 							}
 						}
